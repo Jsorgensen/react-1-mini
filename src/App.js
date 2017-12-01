bcc964ec79a616a98339660cf97ecd875f29eca6
Mike Jorgensen <mikeljorgensen@gmail.com>
2017-12-01 12:24:40 -0700

    mini project completed

--- src/App.js
+++ src/App.js
@@ -3,14 +3,43 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      friends: [],
+      picture: '',
+      name: '',
+      friendsList: []
+    }
+  }
+  updateState(key, value){
+    this.setState({
+      [key]: value
+    })
+  }
+  addFriend(friend){
+    this.setState({
+      friends: [...this.state.friends, {picture: this.state.picture, name: this.state.name}],
+      picture: '',
+      name: '',
+    })
+  }
   render() {
+    const friends = this.state.friends.map(
+      friend => 
+      <div>
+        <img width='300px' src={friend.picture} alt={friend.picture + ', Not Found'}></img>
+        <span>{friend.name}</span>
+      </div>
+    )
     return (
       <div className="App">
         Picture:
-        <input/>
+        <input onChange={(e)=>this.updateState('picture', e.target.value)} value={this.state.picture}/>
         Name:
-        <input/>
-        <button>Add Friend</button>
+        <input onChange={(e)=>this.updateState('name', e.target.value)} value={this.state.name}/>
+        <button onClick={(e)=>this.addFriend()}>Add Friend</button>
+        {friends}
       </div>
     );
   }
