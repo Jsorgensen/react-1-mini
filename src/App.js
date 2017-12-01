2cbae815d87f2394d51306b6df90daf7f7a10f93
Mike Jorgensen <mikeljorgensen@gmail.com>
2017-12-01 11:35:04 -0700

    just started

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Picture:
+        <input/>
+        Name:
+        <input/>
+        <button>Add Friend</button>
+      </div>
+    );
+  }
+}
+
+export default App;
